ae6e08bb1e4f9c2b40f03ce673eece0ba28187e7
VooDooGrayWOOD <voodoograywood@gmail.com>
2023-02-05 16:15:09 +0700

    Revert "add Footer"
    This reverts commit 0f2de3ba

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,64 +1,41 @@
 <template>
-    <div class="container">
-        <HeroSection></HeroSection>
-        <!-- <TrendingCollections></TrendingCollections> -->
-        <!-- <TopCreators></TopCreators> -->
-        <!-- <BrowseCategories></BrowseCategories> -->
-        <!-- <DiscoverMoreNFTS></DiscoverMoreNFTS> -->
-        <!-- <NftHighlight></NftHighlight> -->
-        <!-- <HowItWorks></HowItWorks> -->
-        <!-- <SubscribeWidget></SubscribeWidget> -->
-        <!-- <Footer></Footer> -->
-    </div>
+  <div>
+    <HeroSection></HeroSection>
+    <TrendingCollections></TrendingCollections>
+    <TopCreators></TopCreators>
+    <BrowseCategories></BrowseCategories>
+    <DiscoverMoreNFTS></DiscoverMoreNFTS>
+    <NftHighlight></NftHighlight>
+    <HowItWorks></HowItWorks>
+    <SubscribeWidget></SubscribeWidget>
+  </div>
 </template>
 
 <script>
-import HeroSection from '../components/HeroSection.vue'
+import HeroSection from '../components/HeroSection.vue';
 import TrendingCollections from '../components/TrendingCollections.vue'
-import TopCreators from '../components/TopCreators.vue'
-import BrowseCategories from '../components/BrowseCategories.vue'
-import DiscoverMoreNFTS from '../components/DiscoverMoreNFTS.vue'
-import NftHighlight from '../components/NftHighlight.vue'
-import HowItWorks from '../components/HowItWorks.vue'
-import SubscribeWidget from '../components/SubscribeWidget.vue'
-import Footer from '../components/Footer.vue'
+import TopCreators from "../components/TopCreators.vue";
+import BrowseCategories from "../components/BrowseCategories.vue";
+import DiscoverMoreNFTS from "../components/DiscoverMoreNFTS.vue";
+import NftHighlight from "../components/NftHighlight.vue";
+import HowItWorks from "../components/HowItWorks.vue";
+import SubscribeWidget from "../components/SubscribeWidget.vue";
+
 
 export default {
-    components: {
-        Footer,
-        SubscribeWidget,
-        HowItWorks,
-        NftHighlight,
-        DiscoverMoreNFTS,
-        BrowseCategories,
-        TopCreators,
-        HeroSection,
-        TrendingCollections
-    }
+  components: {
+    SubscribeWidget,
+    HowItWorks,
+    NftHighlight,
+    DiscoverMoreNFTS,
+    BrowseCategories,
+    TopCreators,
+    HeroSection,
+    TrendingCollections
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-.container {
-    display: grid;
-    grid-template-columns: repeat(12, 1fr);
-    gap: 10px;
-    margin: 0 auto;
-    padding: 80px 115px;
-    max-width: 1280px;
-    height: 0;
-}
-
-@media screen and (max-width: 1000px) {
-    .container {
-        padding: 80px 70px;
-    }
-}
-
-@media screen and (max-width: 480px) {
-    .container {
-        padding: 40px 20px;
-    }
-}
 
 </style>
